c32b882cc94c580ddad6ed8391b86d0bd2bf2e85
verma-kartik <kartikv.vero058@gmail.com>
2023-07-10 02:09:14 +0530

    started with Orders.Domain

---
+++ Services/Delivery/Delivery.API/Controllers/DeliveryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+[Route("[controller]")]
+public class DeliveryController : ControllerBase
+{
+    private readonly ILogger<DeliveryController> _logger;
+
+    public DeliveryController(ILogger<DeliveryController> logger)
+    {
+        _logger = logger;
+    }
+}
